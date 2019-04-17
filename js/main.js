9cd7c38b84627cf8fe262e8d7b04f248d6f65af6
kaiqueAMoraes <k.andrades@hotmail.com.br>
2019-04-17 18:03:09 -0300

    conteudo do index adicionado, animacao fade up, e roll in

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).mousemove(function (e) {
     $('.cursor')
         .eq(0)
         .css({
-            left:e.clientX,
+            left: e.clientX,
             top: e.clientY
         });
     setTimeout(function () {
@@ -15,14 +15,33 @@ $(document).mousemove(function (e) {
     }, 100); // 100 representa o numero em milisegundo de delay de seguida.
 });
 
-window.onload = function(){
-    ScrollReveal({ distance: '50px' });
-    ScrollReveal().reveal('#voltar', { delay: 100});
-    ScrollReveal().reveal('#logoImagem', { delay: 200});
-    ScrollReveal().reveal('#usuario', { delay: 500});
-    ScrollReveal().reveal('#senha', { delay: 650});
-    ScrollReveal().reveal('#refazerSenha', { delay: 1000});
+window.onload = function () {
+    var mostrarObjeto = {
+        display: 'none'
+    };
 
-    
+    ScrollReveal({
+        distance: '50px'
+    });
+    ScrollReveal().reveal('#voltar', {
+        delay: 100
+    });
+    ScrollReveal().reveal('#logoImagem', mostrarObjeto, {
+        delay: 200
+    });
+    ScrollReveal().reveal('#usuario', {
+        delay: 500
+    });
+    ScrollReveal().reveal('#senha', {
+        delay: 650
+    });
+    ScrollReveal().reveal('#refazerSenha', {
+        delay: 1000
+    });
+    ScrollReveal().reveal('.animacaoFadeUp', {
+        delay: 350
+    });
+
+   
 
 }
