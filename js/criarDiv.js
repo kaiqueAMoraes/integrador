2224712c58d7a229ea647d1e00fdca8109208fb2
kaiqueAMoraes <k.andrades@hotmail.com.br>
2019-05-30 06:32:53 -0300

    js comentado

--- js/criarDiv.js
+++ js/criarDiv.js
@@ -6,11 +6,11 @@ window.onload = function(){
 
     if(confirm("Deseja adicionar um novo album na area de Selecionados?") == true){
 
-            var div = document.createElement('div');
-            div.className = 'album';
-            div.onclick = "location.href='newurl.html';";
-            document.getElementsByClassName('container')[0].appendChild(div);
-            document.getElementsByClassName('container')[0].append(div);
+            // var div = document.createElement('div');
+            // div.className = 'album';
+            // div.onclick = "location.href='newurl.html';";
+            // document.getElementsByClassName('container')[0].appendChild(div);
+            // document.getElementsByClassName('container')[0].append(div);
             location.href="../paginas/novoAlbum.html";
 
     }
