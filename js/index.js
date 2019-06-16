db995f4b5494562bed02e9d0f148ddf6a113de56
kaiqueAMoraes <k.andrades@hotmail.com.br>
2019-06-16 04:51:07 -0300

    pagina index remodelada e nova pagina trabalhos selecionados

--- js/index.js
+++ js/index.js
@@ -1,25 +1,51 @@
 
 
+
   document.getElementsByClassName('contato')[1].style.left = '5vw';
   document.getElementsByClassName('contato')[2].style.left = '10vw';
 
-   trabalhosSelecionados = document.getElementsByTagName('div')[1];
-   portifolioCompleto = document.getElementsByTagName('div')[2];
+   DivTrabalhosSelecionados = document.getElementsByTagName('div')[1];
+   DivPortifolioCompleto = document.getElementsByTagName('div')[2];
 
-document.getElementsByTagName('a')[1].onmouseenter = function(){
-  trabalhosSelecionados.style.background = '#000';
-  document.getElementsByTagName('a')[0].innerHTML = "Portifolio Completo";
-}
-document.getElementsByTagName('a')[1].onmouseout = function(){
-  trabalhosSelecionados.style.background = '#f5f5f5';
-  document.getElementsByTagName('a')[0].innerHTML = "Trabalhos selecionados";
-}
 
-document.getElementsByTagName('a')[0].onmouseenter = function(){
-  portifolioCompleto.style.background = '#f5f5f5';
-document.getElementsByTagName('a')[1].innerHTML = "Trabalhos Selecionados";
-}
-document.getElementsByTagName('a')[0].onmouseout = function(){
-  portifolioCompleto.style.background = '#000';
-  document.getElementsByTagName('a')[1].innerHTML = "portifolio Completo";
-}
+  PreencherDivTrabalhosSelecionados = function(){
+    DivPortifolioCompleto.style.width = "100vw";
+    DivPortifolioCompleto.style.position = "absolute";
+    DivPortifolioCompleto.style.right = "-50vw";
+
+    document.getElementsByTagName('a')[0].innerHTML = "Portifolio completo";
+  }
+
+  PreencherDivTrabalhosSelecionadosReverso = function(){
+    DivPortifolioCompleto.style.width = ("50vw");
+    DivTrabalhosSelecionados.style.backgroundPosition = "left bottom";
+    document.getElementsByTagName('a')[0].innerHTML = "Trabalhos selecionados";
+  };
+
+  PreencherDivPortifolioCompleto = function(){
+    DivTrabalhosSelecionados.style.width = "100vw";
+    document.getElementsByTagName('a')[1].innerHTML = "Trabalhos selecionados";
+    document.getElementsByTagName('a')[1].style.position = "absolute";
+    document.getElementsByTagName('a')[0].style.zIndex = "99999";
+    document.getElementsByTagName('a')[1].style.opacity = "0";
+
+
+  }
+
+  PreencherDivPortifolioCompletoReverso = function(){
+    document.getElementsByTagName('a')[0].style.zIndex = "0";
+    DivTrabalhosSelecionados.style.width = "50vw";
+    document.getElementsByTagName('a')[1].innerHTML = "Portifolio completo";
+document.getElementsByTagName('a')[0].style.opacity = "100";
+    document.getElementsByTagName('a')[1].style.position = "relative";
+document.getElementsByTagName('a')[1].style.opacity = "100";
+
+  }
+
+  var delay = 1500;
+
+  document.getElementsByTagName('a')[1].onmouseenter = PreencherDivTrabalhosSelecionados;
+  document.getElementsByTagName('a')[1].onmouseout = PreencherDivTrabalhosSelecionadosReverso;
+
+  document.getElementsByTagName('a')[0].onmouseenter = PreencherDivPortifolioCompleto;
+  document.getElementsByTagName('a')[0].onmouseout = PreencherDivPortifolioCompletoReverso;
