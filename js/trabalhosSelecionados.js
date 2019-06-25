dd95385fd5ab972ed60e06f0068b6a3b9fd2d1b7
kaiqueAMoraes <k.andrades@hotmail.com.br>
2019-06-25 17:50:54 -0300

    sass ramdom padding adicionado, imagens e sections adicionados também

---
+++ js/trabalhosSelecionados.js
@@ -0,0 +1,24 @@
+// var jqueryScrollify = require("jquery-scrollify": "1.0.20");
+// var jquery = require("jquery": "3.4.1");
+//
+// $(function() {
+//   $.scrollify({
+//       section : "section",
+//       sectionName : "section-name",
+//       interstitialSection : "",
+//       easing: "easeOutExpo",
+//       scrollSpeed: 1100,
+//       offset : 0,
+//       scrollbars: true,
+//       standardScrollElements: "",
+//       setHeights: true,
+//       overflowScroll: true,
+//       updateHash: true,
+//       touchScroll:true,
+//       before:function() {},
+//       after:function() {},
+//       afterResize:function() {},
+//       afterRender:function() {}
+//     });
+//
+//         });
