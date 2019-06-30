22766b37e43eef778e08bcec90d1c48b21712597
VitorAnjosSantos <vitor.anjos.santoss@gmail.com>
2019-06-30 06:53:28 -0300

    Contato e Sobre

---
+++ paginas/sobre.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+  <html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina Administrador</title>
+    <link rel="stylesheet" type="text/css" href="../sass/css/paginaAdmnistrador.css">
+  </head>
+  <body>
+    <header>
+      <div id="voltar" title="Voltar á pagina anterior">
+        <a href="../index.html"><span>&#60;</span></a>
+      </div>
+    </header>
+  </body>
