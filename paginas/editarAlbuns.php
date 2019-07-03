181ed402a5cf4ebff03844f35fe7ebb656ecd463
kaiqueAMoraes <k.andrades@hotmail.com.br>
2019-07-03 14:48:16 -0300

    merge conflicts

--- paginas/editarAlbuns.php
+++ paginas/editarAlbuns.php
@@ -30,7 +30,7 @@ session_start();
     <link rel="stylesheet" type="text/css" href="../sass/css/editarAlbuns.css">
     <script src="../js/backgroundChiado.js" charset="utf-8"></script>
     <script src="../node_modules/jquery/dist/jquery.min.js"></script>
-    
+
   </head>
 
 
@@ -69,35 +69,28 @@ session_start();
       <div class="button" id="button" title="Criar um novo album"><span>&#x2b;</span></div>
       <section class="container">
         <?php
-<<<<<<< HEAD
+
 //echo "<form method='post' action='../objetos/obj_deleteAlbum.php'>";
 while($album = mysqli_fetch_assoc($result)){
- echo "<div class='albumEditar' >";
 
-            echo "<a title='excluir album.' class='deleteButton' href='../objetos/obj_deleteAlbum.php?id_album=".$album['id_album']."'><span>X</span></a>";
-=======
-
-          echo "<div>";
+          echo "<div class='albumEditar'>";
 
           while($album = mysqli_fetch_assoc($result)){
-            echo "<a href='../objetos/obj_deleteAlbum.php?id_album=".$album['id_album']."&dir=".$album['dir']."'>Excluir</a>";
->>>>>>> master
+            echo "<a title='excluir album.' class='deleteButton' href='../objetos/obj_deleteAlbum.php?id_album=".$album['id_album']."&dir=".$album['dir']."'><span>X</span></a>";
+
 
           echo "<a href= 'album.php?id_album=".$album['id_album']."'><div
                 style=\"background-image: url('../albuns/".$album['dir']."/".$album['arq']."');
                 background-repeat: no-repeat; background-size: cover; background-position: center;
                 filter: grayscale(100%); \" class='albumEditar' >
                    </div></a>";
-<<<<<<< HEAD
-          echo "</div>";
-        }
-=======
+
         }
   //<input type='hidden' name='excluir' id='".$album['id_album']."' value='".$album['id_album']."'>
 
              echo "</div>";
 
->>>>>>> master
+
         ?>
       </section>
     </main>
