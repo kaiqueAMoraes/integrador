b58d0689b7eea99599918968ee1540bf14c489f8
kaiqueAMoraes <k.andrades@hotmail.com.br>
2019-07-03 14:00:30 -0300

    sobremim page

--- paginas/sobreMim.php
+++ paginas/sobreMim.php
@@ -3,8 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title>Sobre mim</title>
+    <link type="text/css" rel="stylesheet" href="../sass/css/sobreMim.css">
   </head>
   <body>
-
+    <header>
+      <div id="voltar" title="Voltar á pagina anterior">
+        <a href="../index.html"><span>&#60;</span></a>
+      </div>
+    </header>
+<main>
+  <div class="containerInfoAlbum">
+    <h1></h1>
+    <p> </p>
+  </div>
+</main>
   </body>
 </html>
