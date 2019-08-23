6b15dcfee6db0cd22bbc1f1af63760a8a37e4d5c
Vitor <vitor.anjos.santoss@gmail.com>
2019-08-22 22:09:50 -0300

    Consertando o upload de imagens

--- objetos/obj_upload_album.php
+++ objetos/obj_upload_album.php
@@ -20,6 +20,8 @@ else {
 
     mkdir($diretorio, 0777);
 
+    $sucessoUpload = true;
+
     $sql = "INSERT INTO tb_album(nome,descricao) VALUES ('".$nomeAlbum."','".$descricaoAlbum."');";
     if(mysqli_query($conexao, $sql)) {
         $idAlbum = mysqli_insert_id($conexao);
@@ -27,7 +29,6 @@ else {
         if (isset($_FILES["capa"])){
           $capa_tmp = $_FILES["capa"]["tmp_name"][0];
           $nomeCapa = $_FILES["capa"]["name"][0];
-
           $somenteNomeCapa = md5(Date("dmYHis").pathinfo($nomeCapa, PATHINFO_FILENAME));
           $somenteExtensaoCapa = pathinfo($nomeCapa, PATHINFO_EXTENSION);
           $nomeCapaCriptografado = "$somenteNomeCapa.$somenteExtensaoCapa";
@@ -37,10 +38,11 @@ else {
 
           if ($uploadCapa) {
               $sql = "INSERT INTO tb_foto(id_album, nome, foto_capa) VALUES ($idAlbum, '".$nomeCapaCriptografado."', $foto_capa);";
-              $resultado= mysqli_query($conexao, $sql);
+              mysqli_query($conexao, $sql);
 
          }
           else {
+              $sucessoUpload = false;
               echo "Erro no upload!";
           }
 
@@ -50,7 +52,6 @@ else {
             // fazer o for para o multiple
             for ($i = 0; $i < count($_FILES["foto"]["name"]); $i++)
             {
-
                 $foto_tmp = $_FILES["foto"]["tmp_name"][$i];
                 $nomeFoto = $_FILES["foto"]["name"][$i];
 
@@ -61,15 +62,15 @@ else {
                 $uploadFoto = move_uploaded_file($foto_tmp, $diretorio . "/" . $nomeFotoCriptografado);
                 if ($uploadFoto) {
                     $sql = "INSERT INTO tb_foto(id_album, nome) VALUES ($idAlbum, '".$nomeFotoCriptografado."');";
-
+                    mysqli_query($conexao, $sql);
                 }
                 else {
+                    $sucessoUpload = false;
                     echo "Erro no upload!";
                 }
 
             }
         }
-
 
         if(isset($_POST["tag"]) != ""){
 
@@ -86,12 +87,17 @@ else {
             }
 
         }
-        if (mysqli_query($conexao, $sql)){
+
+        if ($sucessoUpload){
             session_start();
             $_SESSION["upload"] = true;
 
             header("location:../paginas/novoAlbum.php");
+        } else {
+            session_start();
+            $_SESSION["erro"] = true;
 
+            header("location:../paginas/novoAlbum.php");
         }
 
     }
